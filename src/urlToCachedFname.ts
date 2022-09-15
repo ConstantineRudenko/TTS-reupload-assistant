55f1346acd9e57a7579c5f50cad8d5c395cc2fbb
MadCake <->
2022-09-15 04:10:29 +0200

    inverted url clean regex (whitelist)

--- src/urlToCachedFname.ts
+++ src/urlToCachedFname.ts
@@ -1,4 +1,4 @@
-const regexUrlCleanup = /[:\/\-\.\?=]/g;
+const regexUrlCleanup = /[^a-zA-Z0-9]/g;
 
 export default function urlToFname(url: string): string {
     return url.replace(regexUrlCleanup, "");
