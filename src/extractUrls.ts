7e02ce55671d809abcb84edc5171a77aff6bd9d8
MadCake <placeholder@email.com>
2024-12-19 22:05:32 +0100

    add windows-specific "\" into url regex

--- src/extractUrls.ts
+++ src/extractUrls.ts
@@ -1,11 +1,19 @@
 export default function extractUrls(saveFileContent: string): string[] {
-    const regexUrl = /URL": "([a-zA-Z0-9._~:/?#@!$&'()*+,;%=\-[\]\s]+)"/g;
+	const regexUrl =
+		/(URL|Url|url)": "(?<url>(\\\\|[a-zA-Z0-9._~:/?#@!$&'()*+,;%=\-[\]\s])+)"/g;
 
-    return [
-        ...new Set(
-            Array.from(saveFileContent.matchAll(regexUrl), function (match) {
-                return match[1];
-            })
-        ),
-    ];
+	return [
+		...new Set(
+			Array.from(saveFileContent.matchAll(regexUrl), function (match) {
+				if (match.groups) {
+					const value = match.groups['url'];
+					if (value) {
+						return [value];
+					}
+					return [];
+				}
+				return [];
+			}).flat()
+		),
+	];
 }
