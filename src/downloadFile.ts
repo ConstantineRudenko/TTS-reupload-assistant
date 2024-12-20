31e1ea2f973d7f6e58312390517142f6dc48f0d4
MadCake <placeholder@email.com>
2024-12-20 04:18:30 +0100

    modernize async

--- src/downloadFile.ts
+++ src/downloadFile.ts
@@ -1,133 +1,80 @@
 import * as Log from './logger';
 import { Args } from './parseArgs';
-import fs from 'fs';
 import fsPromises from 'fs/promises';
 import http from 'http';
 import https from 'https';
 import { URL } from 'url';
+import { pipeline } from 'stream/promises';
 
-export default async function downloadFile(
-    filePath: string,
-    url: string,
-    urlIndex: number,
-    args: Args
+async function downloadRemoteFile(
+	filePath: string,
+	url: string,
+	urlIndex: number,
+	args: Args
 ) {
-    let protocol: typeof http | typeof https | undefined = undefined;
-
-    Log.withUrl(url, urlIndex, 'started downloading');
-
-    switch (true) {
-        case url.slice(0, 7) == 'http://':
-            protocol = http;
-            break;
-        case url.slice(0, 8) == 'https://':
-            protocol = https;
-            break;
-        case url.slice(0, 8) == 'file:///':
-            if (args.noLinks) {
-                await fsPromises.copyFile(url.slice(8), filePath);
-            } else {
-                await fsPromises.symlink(url.slice(8), filePath);
-            }
-
-            Log.withUrl(url, urlIndex, 'picked local file');
-
-            return;
-        default:
-            return;
-    }
-
-    await new Promise((resolve) => {
-        protocol!
-            .get(
-                url,
-                // https://github.com/nodejs/node/issues/39341
-                {
-                    timeout: args.timeout,
-                    //headers: { Connection: "keep-alive" },
-                },
-                (response) => {
-                    void (async () => {
-                        await urlResponse(
-                            response,
-                            url,
-                            urlIndex,
-                            args,
-                            filePath
-                        );
-                        resolve(null);
-                    })();
-                }
-            )
-            .on('error', (err) => {
-                Log.withUrl(url, urlIndex, `download error: ${err.message}`);
-                resolve(null);
-            });
-    });
+	const abort = new AbortController();
+	setTimeout(() => {
+		abort.abort();
+	}, args.timeout);
+	const response = await fetch(url, { signal: abort.signal });
+	if (!response.ok) {
+		return;
+	}
+	const text = await response.text();
+	await fsPromises.writeFile(filePath, text, 'utf-8');
 }
 
-async function urlResponse(
-    response: http.IncomingMessage,
-    url: string,
-    urlIndex: number,
-    args: Args,
-    filePath: string
+export default async function downloadFile(
+	filePath: string,
+	url: string,
+	urlIndex: number,
+	args: Args
 ) {
-    switch (response.statusCode) {
-        case 200: {
-            const file = fs.createWriteStream(filePath, {
-                autoClose: true,
-            });
-            response.pipe(file);
+	let protocol: typeof http | typeof https | undefined = undefined;
 
-            await new Promise((resolve) => {
-                file.on('finish', () => {
-                    Log.withUrl(url, urlIndex, 'downloaded successfully');
-                    resolve(null);
-                });
+	Log.withUrl(url, urlIndex, 'started downloading');
 
-                file.on('error', (err) => {
-                    Log.withUrl(url, urlIndex, `file error: ${err.message}`);
-                    resolve(null);
-                    try {
-                        fs.unlink(filePath, () => {
-                            // do nothing
-                        });
-                    } catch {
-                        // do nothing
-                    }
-                    throw err;
-                });
-            });
+	switch (true) {
+		case url.slice(0, 8) == 'file:///':
+			if (args.noLinks) {
+				await fsPromises.copyFile(url.slice(8), filePath);
+			} else {
+				await fsPromises.symlink(url.slice(8), filePath);
+			}
 
-            return;
-        }
-        case 301:
-        case 302: {
-            Log.withUrl(url, urlIndex, 'file moved');
-            Log.spaced(`old:\n${url}`);
+			Log.withUrl(url, urlIndex, 'picked local file');
 
-            let newUrl: URL;
-            try {
-                newUrl = new URL(response.headers['location'] ?? 'error');
-                Log.spaced(`new:\n${response.headers['location']}`);
-            } catch {
-                try {
-                    const oldUrl = new URL(url);
-                    Log.spaced(`new (local):\n${response.headers['location']}`);
-                    newUrl = new URL(url, oldUrl.host);
-                    Log.spaced(`new:\n${newUrl.href}`);
-                } catch (err) {
-                    Log.spaced(`error: ${(err as Error).message}`);
-                    return;
-                }
-            }
-            await downloadFile(filePath, newUrl.href, urlIndex, args);
-            return;
-        }
-        default:
-            Log.withUrl(url, urlIndex, 'failed download');
-            Log.spaced(`response code: ${response.statusCode}`);
-            return;
-    }
+			return;
+		default:
+			try {
+				await downloadRemoteFile(filePath, url, urlIndex, args);
+
+				Log.withUrl(url, urlIndex, 'downloaded file');
+			} catch (err) {
+				Log.withUrl(url, urlIndex, `download error: ${err.message}`);
+			}
+			return;
+	}
+}
+
+function handleRedirect(oldUrlStr: string, response: http.IncomingMessage) {
+	const responseLocation = response.headers['location'];
+	if (!responseLocation) {
+		throw new Error('empty redirect url');
+	}
+	const newUrlStr = responseLocation;
+
+	try {
+		const newUrl = new URL(newUrlStr);
+		Log.spaced(`new:\n${newUrlStr}`);
+		return newUrl;
+	} finally {
+	}
+
+	const oldUrl = new URL(oldUrlStr);
+	Log.spaced(`new (relative):\n${newUrlStr}`);
+	const newUrl = new URL(newUrlStr, oldUrl.host);
+	Log.spaced(`new:\n${newUrl.href}`);
+
+	return newUrl;
 }
--- src/runDownloadQueue.ts
+++ src/runDownloadQueue.ts
@@ -3,71 +3,69 @@ import * as Log from './logger';
 import { Args } from './parseArgs';
 
 export interface UrlDownloadTask {
-    func: () => Promise<unknown>;
-    url: string;
-    urlIndex: number;
-    started: number;
+	func: () => Promise<unknown>;
+	url: string;
+	urlIndex: number;
+	started: number;
 }
 
 export async function runDownloadTasks(taskArr: UrlDownloadTask[], args: Args) {
-    taskArr = taskArr.slice(); // local copy
-    taskArr.reverse();
+	taskArr = taskArr.slice(); // local copy
+	taskArr.reverse();
 
-    const taskArrActive: UrlDownloadTask[] = [];
+	const taskArrActive: UrlDownloadTask[] = [];
 
-    Log.normal(`queue size: ${taskArr.length}`);
-    Log.normal(`simultaneous downloads: ${args.simultaneous}`);
+	Log.normal(`queue size: ${taskArr.length}`);
+	Log.normal(`simultaneous downloads: ${args.simultaneous}`);
 
-    await Promise.all(
-        Array(args.simultaneous)
-            .fill(0)
-            .map(() =>
-                (async () => {
-                    while (taskArr.length > 0) {
-                        const promiseInfo: UrlDownloadTask = taskArr.pop()!;
-                        taskArrActive.push(promiseInfo);
-                        promiseInfo.started = Date.now();
+	await Promise.all(
+		Array(args.simultaneous)
+			.fill(0)
+			.map(async () => {
+				while (taskArr.length > 0) {
+					const promiseInfo: UrlDownloadTask = taskArr.pop()!;
+					taskArrActive.push(promiseInfo);
+					promiseInfo.started = Date.now();
 
-                        printQueue(taskArrActive);
+					printQueue(taskArrActive);
 
-                        Log.withUrl(
-                            promiseInfo.url,
-                            promiseInfo.urlIndex,
-                            'queued url download'
-                        );
+					Log.withUrl(
+						promiseInfo.url,
+						promiseInfo.urlIndex,
+						'queued url download'
+					);
 
-                        await promiseInfo.func();
+					await promiseInfo.func();
 
-                        Log.withUrl(
-                            promiseInfo.url,
-                            promiseInfo.urlIndex,
-                            'processed URL'
-                        );
+					Log.withUrl(
+						promiseInfo.url,
+						promiseInfo.urlIndex,
+						'processed URL'
+					);
 
-                        taskArrActive.remove(promiseInfo);
-                    }
-                })()
-            )
-    );
+					taskArrActive.remove(promiseInfo);
+				}
+			})
+	);
 }
 
 function printQueue(promiseInfoArrActive: UrlDownloadTask[]) {
-    const now = Date.now();
+	const now = Date.now();
 
-    const numTasks = promiseInfoArrActive.length;
+	const numTasks = promiseInfoArrActive.length;
 
-    if (numTasks > 0) {
-        Log.spaced(`active tasks (${numTasks}):`);
+	if (numTasks > 0) {
+		Log.spaced(`active tasks (${numTasks}):`);
 
-        promiseInfoArrActive.forEach((promiseInfo) => {
-            const sPassed = ((now - promiseInfo.started) / 1000).toFixed();
-            Log.withUrl(
-                promiseInfo.url,
-                promiseInfo.urlIndex,
-                `(${sPassed} seconds ago)`
-            );
-        });
-    } else {
-        Log.normal('no tasks left');
-    }
+		promiseInfoArrActive.forEach((promiseInfo) => {
+			const sPassed = ((now - promiseInfo.started) / 1000).toFixed();
+			Log.withUrl(
+				promiseInfo.url,
+				promiseInfo.urlIndex,
+				`(${sPassed} seconds ago)`
+			);
+		});
+	} else {
+		Log.normal('no tasks left');
+	}
 }
